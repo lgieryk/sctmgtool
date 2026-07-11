d6dc9f881e2139ec7ace3591eaa497ce6e2d5adb
Łukasz Gieryk <108897150+lgieryk@users.noreply.github.com>
2026-07-11 23:18:05 +0200

    Workaround for Matplotlib figure scaling on Windows

--- src/sctmgtool/__main__.py
+++ src/sctmgtool/__main__.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2026 Łukasz Gieryk
 
 import logging
+import ctypes
+import os
 from multiprocessing import freeze_support
 from sctmgtool.tools import process_unit_list
 from sctmgtool.tk import tkinter_main
@@ -9,6 +11,12 @@ from sctmgtool.units import ALL_UNITS
 
 
 if __name__ == "__main__":
+    if os.name == "nt":
+        # Workaround for high resolution displays on Windows with the scaling
+        # enabled. Matplotlib renders with too small font for the figures to be
+        # readable otherwise.
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)  # 2 = per-monitor DPI awareness
+
     freeze_support()
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
     process_unit_list(ALL_UNITS)
